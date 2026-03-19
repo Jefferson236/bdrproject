a01188ad936129f593edfaffa4f20bac5e14a630
Jefferson236 <62144310+Jefferson236@users.noreply.github.com>
2026-03-19 15:53:17 -0400

    icono

--- vista/header.php
+++ vista/header.php
@@ -9,7 +9,7 @@ if (!isset($logo)) { $logo = '🍔'; }
     <title>Hamburguesas Don Pancho | El Sabor de la Parrilla</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="icon" type="image/png" href="favicon.ico">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
